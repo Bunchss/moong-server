9c4be045e58862831dd4cdbf906d0dbfb6a00fba
ten <ljy2784437@gmail.com>
2024-09-06 18:41:32 +0900

    :fire: spring project settings

---
+++ src/main/java/kakao/com/moongserver/MoongServerApplication.java
@@ -0,0 +1,13 @@
+package kakao.com.moongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoongServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kakao/com/moongserver/MoongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.com.moongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoongServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
