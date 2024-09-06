f2834d2f36ab6236ded8611965424a4b80f14ee2
ten <ljy2784437@gmail.com>
2024-09-06 23:09:20 +0900

    :sparkles: WebClient Config

---
+++ src/main/java/kakao/com/moongserver/config/WebClientConfig.java
@@ -0,0 +1,28 @@
+package kakao.com.moongserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.print.attribute.standard.Media;
+import java.time.Duration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("ai 서버 엔드포인트")       // https://localhost:8080
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create().responseTimeout(Duration.ofSeconds(5))
+                ))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+}
